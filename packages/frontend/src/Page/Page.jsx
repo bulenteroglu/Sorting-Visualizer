0a3d56a4b536093acfe380763da33ec50f72c181
Bulent <eroglu.bulent98@gmail.com>
2020-11-06 12:57:23 +0000

    added random array button#

--- packages/frontend/src/Page/Page.jsx
+++ packages/frontend/src/Page/Page.jsx
@@ -1,16 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-function Array() {
-  const [temp, setTemp] = useState([2, 3, 5, 6]);
-
-  useEffect(() => {
-    var randoms = [...Array(4)].map(() => Math.floor(Math.random() * 9));
-    setTemp(randoms);
-  }, []);
-
+function Array({ array }) {
   return (
     <div className='w-full flex justify-between mt-5 px-20 h-full space-x-2'>
-      {temp.map((arr, i) => (
+      {array.map((arr, i) => (
         <div
           style={{ height: `${arr}%` }}
           key={i}
@@ -24,24 +17,36 @@ function Array() {
 }
 
 export default function Page() {
+  const [array, setArray] = useState([1, 2, 8, 4, 3, 2, 7, 5, 3, 21]);
+
+  function randomArray() {
+    const ARRAY_LENGTH = 10;
+    const tempArr = [];
+    for (let i = 0; i < ARRAY_LENGTH; i++) {
+      tempArr.push(Math.floor(Math.random() * 50) + 10);
+    }
+
+    setArray(tempArr);
+  }
+
   return (
     <div>
       <div className='mt-5 flex items-center justify-between text-white rounded'>
-        <div className='bg-orange-500 rounded-lg px-6 py-2 cursor-pointer italic'>
+        <div
+          onClick={randomArray}
+          className='bg-orange-500 rounded-lg px-6 py-2 cursor-pointer italic'
+        >
           Randomise Array
         </div>
         <div>
           <ul className='flex space-x-12'>
-            <li
-              onClick={() => {}}
-              className='bg-indigo-500 rounded-lg px-6 py-2 cursor-pointer'
-            >
+            <li className='bg-indigo-500 rounded-lg px-6 py-2 cursor-pointer'>
               Bubble Sort
             </li>
           </ul>
         </div>
       </div>
-      <Array />
+      <Array array={array} />
     </div>
   );
 }
